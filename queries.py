e3b95b1cf500f27eba7a3650a3e8f99aba06ffec
alexsincai <alex.sincai@yahoo.co.uk>
2021-11-23 14:42:04 +0200

    several parameters

--- queries.py
+++ queries.py
@@ -6,10 +6,20 @@ def get_all_users():
 
 
 def get_user_by_id(id): # <- id = valoarea
-    #   %(cheia)%
+    #   %(cheia)s
     #
     # { "cheia": valoarea }
     return execute_select(
         "select name, password from users where id = %(id)s;", {"id": id}
         # "select name, password from users where id = '7 or 1 = 1; drop table users';"
     )
+
+def selecting_and_updating(id, name):
+    return execute_select("""
+        select username where id = %(id)s;
+        set name = 'gigi' where name = %(name)s;
+        delete from users where id = %(id)s;
+    """, {
+        "id": id,
+        "name": name
+    })
